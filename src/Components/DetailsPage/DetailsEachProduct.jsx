73ee75b9d4cdfa3a17b4826d78e12f0bbc202768
Mohammad Hossein Rezaei <rezaeiism@yahoo.com>
2022-05-30 20:51:24 +0430

    start style for product details step 2 add position sticky for image product in details start handel basic data

--- src/Components/DetailsPage/DetailsEachProduct.jsx
+++ src/Components/DetailsPage/DetailsEachProduct.jsx
@@ -16,7 +16,7 @@ export const DetailsEachProduct = ({EachProduct}) =>
 
         <>
 
-            <div className='container relative h-screen max-w-5xl bg-amber-500 mx-auto'>
+            <div className='container relative h-screen max-w-4xl  mx-auto'>
 
                 <section className='bg-red-700 w-80 h-full absolute left-0'>
 
@@ -25,84 +25,86 @@ export const DetailsEachProduct = ({EachProduct}) =>
                 </section>
 
 
-                <section className='w-3/6 absolute right-0 bg-blue-300 flex flex-col justify-start items-start gap-2 p-6'>
+                <section className='w-3/6 absolute right-0  flex flex-col justify-start items-start gap-2 p-6'>
 
                     <p className='text-3xl font-bold'>Buy {product}</p>
 
-                    <div>
+                    <div className='h-24 flex flex-col justify-between items-start'>
                         <p className='text-lg font-medium'>Get $110–$700 off when you trade in an {product} or newer</p>
                         <p className='text-lg text-blue-700'>See how trade-in works</p>
                     </div>
 
 
-                    <div className='w-48 flex justify-start items-center'>
-                        <p>{rate} / 5</p>
-                        <RatingStar id={id.toString()} rating={rate} size={17}/>
+                    <div className='w-48 h-11 flex justify-start items-center'>
+                        <div className='flex justify-start items-center gap-1'> <p className='text-2xl font-bold'>{rate}</p>/ 5</div>
+                        <RatingStar id={id.toString()} rating={rate} size={20}/>
                     </div>
 
-                    <div>
+                    <div className='h-16 flex flex-col justify-center items-start'>
                         <span className='flex justify-start items-center gap-2 w-48'> <p className='font-bold text-lg'>Category :</p> <p className='text-lg'>{type}</p></span>
                         <span className='flex justify-start items-center gap-2 w-48'> <p className='font-bold text-lg'>Brand :</p> <p className='text-lg'>{brand}</p></span>
                     </div>
 
 
-                    <label htmlFor='select-color' className='text-lg'>Choose your color</label>
-                    <div id='select-color' className='grid grid-cols-2 grid-rows-2 gap-4'>
+                    <label htmlFor='select-color' className='text-xl font-bold mt-4'>Choose your color</label>
 
-                        <div className='w-48 h-28 bg-amber-500 flex flex-col justify-center items-center gap-2 rounded-2xl border-2 border-blue-600'>
+                    <div id='select-color' className='grid pb-7 grid-cols-2 grid-rows-2 gap-4 border-b border-gray-400'>
+                        <div className='w-48 h-28 flex flex-col justify-center items-center gap-2 rounded-xl border-2 border-blue-600'>
                             <div>
                                 <div className='w-9 h-9 bg-red-700 rounded-full'> </div>
                                 <div className='text-center'>color</div>
                             </div>
                         </div>
-                        <div className='w-48 h-28 bg-green-700 flex flex-col justify-center items-center gap-2 rounded-2xl'>
+                        <div className='w-48 h-28  flex flex-col justify-center items-center gap-2 rounded-xl border border-gray-400 '>
                             <div>
                                 <div className='w-9 h-9 bg-red-700 rounded-full'> </div>
                                 <div className='text-center'>color</div>
                             </div>
                         </div>
-                        <div className='w-48 h-28 bg-amber-300 flex flex-col justify-center items-center gap-2 rounded-2xl'>
+                        <div className='w-48 h-28  flex flex-col justify-center items-center gap-2 rounded-xl border border-gray-400 '>
                             <div>
                                 <div className='w-9 h-9 bg-red-700 rounded-full'> </div>
                                 <div className='text-center'>color</div>
                             </div>
                         </div>
-
                     </div>
 
-                    <label htmlFor='select-Capacity' className='text-lg'>Choose your capacity</label>
-                    <p>How much capacity is right for you?</p>
-                    <div id='select-Capacity' className='grid grid-cols-2 grid-rows-2 gap-4'>
+                    <div className='w-full pb-7 flex flex-col justify-center items-start gap-1 mt-3 border-b border-gray-400'>
+                        <label htmlFor='select-Capacity' className='text-lg font-bold'>Choose your capacity</label>
+                        <p className='text-blue-700'>How much capacity is right for you?</p>
+                        <div id='select-Capacity' className='grid grid-cols-2 grid-rows-2 gap-4'>
 
-                        <div className='w-48 h-28 bg-amber-500 flex flex-col justify-center items-center gap-2 rounded-2xl border-2 border-blue-600'>
-                            <div>
-                                <div className=''>
-                                    <p>128</p>
-                                    <p>GB</p>
+                            <div className='w-48 h-28 flex flex-col justify-center items-center gap-2 rounded-2xl border-2 border-blue-600'>
+                                <div>
+                                    <div className='flex justify-center items-center gap-1'>
+                                        <p className='text-3xl'>128</p>
+                                        <p className='text-lg'>GB</p>
+                                    </div>
+                                    <div className='text-center'>from $799</div>
                                 </div>
-                                <div className='text-center'>from $799</div>
                             </div>
-                        </div>
-                        <div className='w-48 h-28 bg-green-700 flex flex-col justify-center items-center gap-2 rounded-2xl'>
-                            <div>
-                                <div className=''>
-                                    <p>128</p>
-                                    <p>GB</p>
+                            <div className='w-48 h-28  flex flex-col justify-center items-center gap-2 rounded-2xl border border-gray-400'>
+                                <div>
+                                    <div className='flex justify-center items-center gap-1'>
+                                        <p className='text-3xl'>128</p>
+                                        <p className='text-lg'>GB</p>
+                                    </div>
+                                    <div className='text-center'>from $799</div>
                                 </div>
-                                <div className='text-center'>from $799</div>
                             </div>
-                        </div>
-                        <div className='w-48 h-28 bg-amber-300 flex flex-col justify-center items-center gap-2 rounded-2xl'>
-                            <div>
-                                <div className=''>
-                                    <p>128</p>
-                                    <p>GB</p>
+                            <div className='w-48 h-28  flex flex-col justify-center items-center gap-2 rounded-2xl border border-gray-400'>
+                                <div>
+                                    <div className='flex justify-center items-center gap-1'>
+                                        <p className='text-3xl'>128</p>
+                                        <p className='text-lg'>GB</p>
+                                    </div>
+                                    <div className='text-center'>from $799</div>
                                 </div>
-                                <div className='text-center'>from $799</div>
                             </div>
-                        </div>
 
+                        </div>
                     </div>
+
 
                     <p>Do you have a smartphone to trade in with Apple?</p>
 
