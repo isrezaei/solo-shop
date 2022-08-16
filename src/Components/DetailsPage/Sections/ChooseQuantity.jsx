7236e23251798a027823460a99ca902485d57342
Mohammad Hossein Rezaei <rezaeiism@yahoo.com>
2022-08-16 20:24:29 +0430

    Responsive => responsive ChooseQuantity.jsx fixed style

--- src/Components/DetailsPage/Sections/ChooseQuantity.jsx
+++ src/Components/DetailsPage/Sections/ChooseQuantity.jsx
@@ -105,7 +105,7 @@ export const ChooseQuantity = () =>
             lg:h-20 lg:my-0 lg:justify-between ${accessQuantity()}`}>
                 <div className={`
                  bg-blue-600 cursor-pointer flex justify-center items-center text-white 
-                 xs:w-56  xs:h-10 xs:rounded-full xs:text-sm 
+                 xs:w-48 xs:h-10 xs:rounded-full xs:text-sm 
                  lg:w-4/6 lg:h-12 lg:rounded-none lg:text-[1rem]
                 ${HaveQuantity(id) ?  'opacity-50 pointer-events-none' : 'cursor-pointer'}`} onClick={handelAdd}>
                     {!quantity ? 'Add to cart' : 'Choose your quantity'}
