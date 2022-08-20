5b7d3d5db78243570408378497796b378791115f
isrezei <rezaeiism@yahoo.com>
2022-08-20 13:23:11 +0430

    Responsive => responsive cart shop list part-2

--- src/Components/CartPage/CartPreRender.jsx
+++ src/Components/CartPage/CartPreRender.jsx
@@ -15,6 +15,7 @@ export const CartPreRender = () =>
     const Render = CartProduct.map(ids => <CartEachProduct key={ids} ids={ids}/>)
     const {totalQuantity , totalPrice} = useSelector(state => state.MasterDataSlice)
     const [ShippingFee , setShippingFee] = useState(5)
+    const [continueShopping , setContinueShopping] = useState(true)
     const [headerPosition] = useState('relative');
     const Navigation = useNavigate()
 
@@ -35,13 +36,16 @@ export const CartPreRender = () =>
 
 
                 <div className='
-                container h-150 max-w-screen-2xl mx-auto flex
+                container h-150 max-w-screen-2xl mx-auto flex relative
                 xs:flex-col
-                lg:flex-row lg:justify-center lg:items-start
+                lg:flex-row lg:justify-evenly lg:items-start
                 '>
 
 
-                    <div className='flex flex-col justify-between items-center xs:w-full lg:w-8/12'>
+                    <div className={`
+                    ${continueShopping ? 'animate__animated animate__bounceInLeft opacity-100' : 'animate__animated animate__backOutLeft opacity-0'}
+                    flex flex-col justify-between items-center xs:w-full lg:w-8/12
+                    `}>
 
                         <div className='xs:w-full xs:h-8 lg:h-20 lg:w-11/12  px-4 flex justify-between items-center border-b border-b-gray-200'>
                             <p className='xs:text-sm lg:text-2xl font-bold text-gray-600'>Shopping Cart</p>
@@ -65,39 +69,56 @@ export const CartPreRender = () =>
                     </div>
 
 
-                    <div className='w-3/12 h-full p-6 bg-gray-100'>
-                        <div className='w-full h-14 flex items-start text-3xl text-gray-600 font-bold border-b border-b-gray-300'>Order Summary</div>
-                        <div className='w-full h-16 flex justify-between items-center'>
-                            <p className=''>ITEMS {totalQuantity}</p>
-                            <p className='text-xl text-gray-700'>${totalPrice.toFixed(2)}</p>
+                    <button onClick={()=> setContinueShopping( value=> !value) } className='xs:block lg:hidden w-44 h-10 rounded-full bg-neutral-100 font-bold text-neutral-500 mx-auto my-2'>continue</button>
+
+
+                    <div className={`
+                    ${continueShopping ? 'animate__animated animate__backOutLeft' : 'animate__animated animate__bounceInLeft'}                   
+                    p-6 bg-gray-100
+                    xs:absolute xs:w-11/12 xs:h-4/5 xs:top-0 xs:left-4 xs:rounded-3xl
+                    lg:relative lg:w-3/12 lg:h-full lg:rounded-none
+                    `}>
+
+
+                        <div className='w-full flex items-start text-gray-600 font-bold border-b border-b-gray-300 xs:h-10 xs:text-lg lg:h-14 lg:text-3xl '>Order Summary</div>
+
+                        <div className='w-full xs:h-12 lg:h-16 flex justify-between items-center'>
+                            <p className='xs:text-sm lg:text-xl'>ITEMS {totalQuantity}</p>
+                            <p className='xs:text-sm lg:text-xl text-gray-700'>${totalPrice.toFixed(2)}</p>
                         </div>
-                        <div className='w-full h-28 flex flex-col justify-center gap-y-1 items-start'>
+
+                        <div className='xs:h-20 lg:h-28 w-full flex flex-col justify-center gap-y-1 items-start'>
                             <label htmlFor='shipping' className='text-gray-500 text-sm'>SHIPPING</label>
-                            <select id='shipping' className='border-none w-full'>
+                            <select id='shipping' className='border-none w-full xs:text-sm xs:rounded-2xl lg:text-[1rem] lg:rounded-none'>
                                 <option>Standard delivery - $5.00</option>
                                 <option>Fast delivery - $20.00</option>
                             </select>
                         </div>
-                        <div  className='w-full h-28 flex flex-col justify-center gap-y-1 items-start'>
+
+                        <div  className='xs:h-20 lg:h-28 w-full flex flex-col justify-center gap-y-1 items-start'>
                             <label htmlFor='promo-code' className='text-gray-500 text-sm'>PROMO CODE</label>
-                            <input id='promo-code' placeholder='inter your code' className='w-full h-11 p-3'/>
+                            <input id='promo-code' placeholder='inter your code' className='w-full h-11 p-3 xs:text-sm xs:rounded-2xl lg:text-[1rem] lg:rounded-none'/>
                         </div>
-                        <div className='w-full h-24  flex items-center border-b border-b-gray-300'>
-                            <button className='w-24 h-11 bg-blue-700 text-white'>APPLY</button>
+                        <div className='xs:h-16 lg:h-24 w-full flex items-center border-b border-b-gray-300'>
+                            <button className='xs:w-20 xs:h-10 lg:w-24 lg:h-11 bg-blue-700 text-white xs:rounded-full xs:text-sm lg:rounded-none lg:text-[1rem]'>APPLY</button>
                         </div>
+
                         <div className='w-full h-16 flex justify-between items-center'>
-                            <p className='text-gray-500'>TOTAL COST</p>
-                            <p className='text-2xl font-bold text-blue-700'>${(totalPrice + ShippingFee).toFixed(2)}</p>
+                            <p className='xs:text-sm lg:text-[1rem] text-gray-500'>TOTAL COST</p>
+                            <p className='xs:text-sm lg:text-2xl font-bold text-blue-700'>${(totalPrice + ShippingFee).toFixed(2)}</p>
                         </div>
-                        <div className='w-full h-56  flex flex-col justify-center gap-3 items-center'>
-                            <button className='w-72 h-11 bg-blue-700 text-white'>Continue & CHECKOUT</button>
-                            <button className='w-72 h-11 bg-red-500 text-white' onClick={clearAndBack}>CLEAR CART & BACK</button>
+
+                        <div className='w-full xs:h-36 lg:h-56  flex flex-col justify-center gap-3 items-center'>
+                            <button className='w-72 xs:h-8 xs:rounded-full xs:text-sm lg:h-11 lg:rounded-none lg:text-[1rem] bg-blue-700 text-white'>Continue & CHECKOUT</button>
+                            <button className='w-72 xs:h-8 xs:rounded-full xs:text-sm lg:h-11 lg:rounded-none lg:text-[1rem] bg-red-500 text-white' onClick={clearAndBack}>CLEAR CART & BACK</button>
                             <div className='w-72 h-11 text-blue-900 flex justify-start items-center gap-2 cursor-pointer'>
-                                <IoMdArrowRoundBack/>
-                                <p>Continue Shopping</p>
+                                <IoMdArrowRoundBack className='xs:text-sm lg:text-[1rem]'/>
+                                <p className='xs:text-sm lg:text-[1rem]'>Continue Shopping</p>
                             </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
             }
